4668aedebc30038fe7768ff73b12a3cf88ff1a24
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-06-08 06:07:24 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -14,8 +14,7 @@ end
 def age_appropriate_birthday(birthday_kids)
  birthday_kids.collect do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-    age = index 
-    if index > 12
+    if age > 12
     puts  "You are too old for this."
    end 
   end 
